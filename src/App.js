efb29bdfe3156f48795da0ae7af786db268c0f21
lucifermorningstar1305 <adityam.ghosh@gmail.com>
2024-03-01 20:37:19 +1300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
-function App() {
+const initialState = {
+  questions: [],
+  //'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+  error: "",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error", error: action.payload };
+
+    default:
+      throw new Error("Unknown action type");
+  }
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    async function getQuestions() {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+        if (!res.ok)
+          throw new Error("Something went wrong with fetching the data.");
+        const data = await res.json();
+        dispatch({ type: "dataRecieved", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed", payload: err.message });
+      }
+    }
+    getQuestions();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1 / 15</p>
+        <p>Question</p>
+      </Main>
     </div>
   );
-}
+};
 
 export default App;
